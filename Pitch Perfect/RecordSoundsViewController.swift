e4570b32361ead9ee5b7339c7db0fb3ffa82afa0
Sheethal Shenoy <sheethalshenoy@Sheethals-MBP.home>
2016-02-09 06:12:58 -0600

    adding pitch perfect changes

---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,92 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Sheethal Shenoy on 2/1/16.
+//  Copyright © 2016 Sheethal Shenoy. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    var audioRecorder:AVAudioRecorder!
+    var recordedAudio:RecordedAudio!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var showStopButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var tapToRecord: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        showStopButton.hidden = true
+        tapToRecord.hidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func stopButton(sender: UIButton) {
+        recordButton.enabled = true
+        recordLabel.hidden = true
+        tapToRecord.hidden = false
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+
+    }
+    
+    @IBAction func recordingButton(sender: UIButton) {
+        recordLabel.hidden = false
+        tapToRecord.hidden = true
+        showStopButton.hidden = false
+        recordButton.enabled = false
+        
+        //Inside func recordAudio(sender: UIButton)
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "my_audio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "stopRecording"){
+            let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
+            let data = sender as! RecordedAudio
+            playSoundsVC.recordedAudio = data
+        }
+    }
+    
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if(flag){
+            recordedAudio = RecordedAudio(filePath: recorder.url, title: recorder.url.lastPathComponent!)
+            //recordedAudio.filePath = recorder.url
+           // recordedAudio.title = recorder.url.lastPathComponent
+            self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
+        }else{
+            print("Recording not successful")
+            recordButton.enabled = false
+            showStopButton.hidden = true
+        }
+    }
+    
+}
+
---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,13 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Sheethal Shenoy on 2/7/16.
+//  Copyright © 2016 Sheethal Shenoy. All rights reserved.
+//
+
+import UIKit
+
+class RecordedAudio: NSObject {
+
+}
