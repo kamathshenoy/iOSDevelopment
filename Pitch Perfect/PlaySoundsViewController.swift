411ff90e96057a7c37c2c9c69cb5644fd50df836
Sheethal Shenoy <sheethalshenoy@Sheethals-MBP.home>
2016-02-09 07:51:49 -0600

    dding chnage

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,21 +7,78 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
+    var avplayer: AVAudioPlayer!
+    var recordedAudio : RecordedAudio!
+    var engine: AVAudioEngine!
+    var audiofile: AVAudioFile!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        try! avplayer = AVAudioPlayer(contentsOfURL: recordedAudio.filePath)
+        avplayer.enableRate = true
+        
+        engine = AVAudioEngine()
+        try! audiofile = AVAudioFile(forReading: recordedAudio.filePath)
+        
     }
     
+    
+    func playAudioWithVariablePitch(pitch:Float) {
+        avplayer.stop()
+        engine.stop()
+        engine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        engine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        engine.attachNode(changePitchEffect)
+        print("play chipmunk audio pitch", pitch)
+        engine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        engine.connect(changePitchEffect, to: engine.outputNode, format: nil)
+        audioPlayerNode.scheduleFile(audiofile, atTime: nil, completionHandler: nil)
+        try! engine.start()
+        audioPlayerNode.play()
+        
+    }
 
+    @IBAction func stopAudio(sender: AnyObject) {
+        avplayer.stop()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+       
+    @IBAction func playSlow(sender: AnyObject) {
+        avplayer.currentTime = 0.0
+        stopAndPlay(0.5)
+    }
+    
+    @IBAction func playDarthvaderAudio(sender: AnyObject) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    @IBAction func playChipmunkAudio(sender: AnyObject) {
+        print("play chipmunk audio")
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func stopAndPlay(varB:Float){
+        avplayer.stop()
+        avplayer.rate = varB
+        avplayer.play()
+    }
+
+    @IBAction func playFast(sender: AnyObject) {
+        avplayer.currentTime = 0.0
+        stopAndPlay(2.0)
+    }
+   
     /*
     // MARK: - Navigation
 
